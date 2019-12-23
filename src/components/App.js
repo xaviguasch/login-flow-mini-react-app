98426726ce43f4ba80d4cb60dee4da19857c3062
xaviguasch <xaviguasch@gmail.com>
2019-12-23 13:12:28 +0100

    refactored conditional rendering to make it cleaner

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Form from './Form'
-import Login from './Login'
 
 var userIsRegistered = false
 
 function App() {
     return (
         <div className='container'>
-            {userIsRegistered ? <Login /> : <Form />}
+            <Form userIsRegistered={userIsRegistered} />
         </div>
     )
 }
